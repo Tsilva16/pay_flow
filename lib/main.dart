49dd556f837de26c1f0aa268182c4f1d31ad9924
Tsilva16 <tisolesilva@gmail.com>
2021-06-22 20:53:57 -0300

    splash page

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(
+        primaryColor: AppColors.primary
+      ),
+      home: SplashPage(),
+    );
+  }
+}
+
