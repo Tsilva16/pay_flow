53ee8ecc60f64ab8b1c0c39545c9da0b204bb774
Tsilva16 <tisolesilva@gmail.com>
2021-06-24 15:41:26 -0300

    Conf Firebase

---
+++ lib/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'modules/login/login_page.dart';
+import 'shared/themes/app_colors.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(
+        primaryColor: AppColors.primary
+      ),
+      home: LoginPage(),
+    );
+  }
+}
--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -55,9 +56,22 @@ class _LoginPageState extends State<LoginPage> {
                 ),
                 Padding(
                   padding: const EdgeInsets.only(left: 40, right: 40, top: 40),
-                  child: SocialLoginButton(onTap: (){
-                    print("Clicou");
-                  },),
+                  child: SocialLoginButton(
+                    onTap: () async {
+                    GoogleSignIn _googleSignIn = GoogleSignIn(
+                      scopes: [
+                       'email',
+                       'https://www.googleapis.com/auth/contacts.readonly',
+                      ],
+                    );
+                    try {
+                      final response = await _googleSignIn.signIn();
+                      print(response);
+                    } catch (error) {
+                        print(error);
+                    }
+                  },
+                  ),
                 )
               ],
               ),
