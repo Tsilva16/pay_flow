ee5cf90bba7c0a9646c59cc22e0f4009cdcc47cf
Tsilva16 <tisolesilva@gmail.com>
2021-06-24 20:49:29 -0300

    AttRoutes

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -10,7 +10,7 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(primaryColor: AppColors.primary),
-      initialRoute: "splash",
+      initialRoute: "/splash",
       routes: {
         "/splash" : ( context) => SplashPage(),
         "/home" :(context) => HomePage(),
