c7ce35fae527d38df48e132ff218b105c717438b
Tsilva16 <tisolesilva@gmail.com>
2021-06-24 17:09:19 -0300

    Create AuthController

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
 import 'modules/login/login_page.dart';
 import 'shared/themes/app_colors.dart';
 
@@ -7,10 +9,13 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pay Flow',
-      theme: ThemeData(
-        primaryColor: AppColors.primary
-      ),
-      home: LoginPage(),
+      theme: ThemeData(primaryColor: AppColors.primary),
+      initialRoute: "splash",
+      routes: {
+        "/splash" : ( context) => SplashPage(),
+        "/home" :(context) => HomePage(),
+        "/login" : (context) => LoginPage(),
+      },
     );
   }
 }
---
+++ lib/shared/auth/auth_controller.dart
@@ -0,0 +1,24 @@
+import 'dart:js';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/login/login_page.dart';
+
+class AuthController{
+  var _isAuthenticated = false;
+  var _user;
+
+  get user => _user;
+   
+  void setUser(BuildContext context, var user){
+    if(user != null){
+      _user = user;
+      _isAuthenticated = true;
+      Navigator.popAndPushNamed(context, "/home" );
+    } else {
+      _isAuthenticated = false;
+      Navigator.popAndPushNamed(context, "/login");
+    }
+  }
+}
